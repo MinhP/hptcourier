6b0f76c5165a07bbee9b0be5f56b0b3a1ef26e65
Benedict Minh Phan <benedict.phan@gmail.com>
2015-01-12 14:12:04 -0800

    fixed ordering

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  default_scope { order('id DESC') }
   has_many :orderitems
   belongs_to :event
   belongs_to :user
--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -1,4 +1,5 @@
 class Orderitem < ActiveRecord::Base
+  default_scope { order('id ASC') }
   belongs_to :order
   belongs_to :item
 end

c22b28da47017e356634b1d132f409957b8e9e89
Benedict Minh Phan <benedict.phan@gmail.com>
2015-02-15 22:23:38 -0800

    fixed assets pipeline and code to view currency

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+Rails.application.config.assets.precompile += %w( events.js users.js )
+# Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+
+#Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+#Rails.application.config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/ 

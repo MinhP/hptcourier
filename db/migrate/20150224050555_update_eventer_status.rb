dd9a8a6fee83dc5c7eaae8857188ba1c9beecd33
Benedict Minh Phan <benedict.phan@gmail.com>
2015-02-23 22:42:24 -0800

    migration fixed

--- db/migrate/20150224050555_update_eventer_status.rb
+++ db/migrate/20150224050555_update_eventer_status.rb
@@ -1,7 +1,7 @@
 class UpdateEventerStatus < ActiveRecord::Migration
   def self.up
     change_table :eventers do |t|
-      t.change :event_status, 'integer USING CAST(column_name AS integer)'
+      t.change :event_status, 'integer USING CAST(event_status AS integer)'
     end
   end
   def self.down
